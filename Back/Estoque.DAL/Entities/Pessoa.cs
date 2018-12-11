2444b72c7fded11f69cb63848fa6196f69878809
ErikSantosDev <eriksantoz@icloud.com>
2018-12-10 22:29:52 -0200

    Add: Classes, context, gitignore

--- Back/Estoque.DAL/Entities/Pessoa.cs
+++ Back/Estoque.DAL/Entities/Pessoa.cs
@@ -1,7 +1,21 @@
-﻿namespace Estoque.DAL.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Estoque.DAL.Entities
 {
     public class Pessoa: Entity
     {
         public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataDeNascimento { get; set; }
+
+        public string Endereco { get; set; }
+
+        public long CidadeId { get; set; }
+
+        public virtual Cidade Cidade { get; set; }
     }
 }
